3da6ba72a4acca169909f83c8331e1ee81cd37d1
romit <romit@192.168.0.212>
2024-04-01 16:18:44 +0100

    Updated

--- Seleniumbasics/src/main/java/org/selenium/Browser_launch.java
+++ Seleniumbasics/src/main/java/org/selenium/Browser_launch.java
@@ -2,14 +2,26 @@ package org.selenium;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class Browser_launch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		WebDriver driver=new ChromeDriver();
+		 WebDriver driver=new ChromeDriver();
+		 WebDriver driver1=new FirefoxDriver();
+		// driver1.get("https://demowebshop.tricentis.com/");
 		driver.get("https://demowebshop.tricentis.com/");
 		driver.manage().window().maximize();
+		String title=driver.getTitle();
+		System.out.println(title);
+		String currenturl=driver.getCurrentUrl();
+		System.out.println(currenturl);
+		String windowhandle=driver.getWindowHandle();
+		System.out.println(windowhandle);
+		String pagesource=driver.getPageSource();
+		System.out.println(pagesource);
 		//driver.close();
 
 	}
