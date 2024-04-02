5803898246ee6b642eea718a63ac42c17ba21993
romit <romit@192.168.0.212>
2024-04-02 20:37:18 +0100

    Commit

--- Seleniumbasics/src/main/java/org/selenium/Browser_launch.java
+++ Seleniumbasics/src/main/java/org/selenium/Browser_launch.java
@@ -2,6 +2,7 @@ package org.selenium;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
@@ -11,7 +12,12 @@ public class Browser_launch {
 		// TODO Auto-generated method stub
 		 WebDriver driver=new ChromeDriver();
 		 WebDriver driver1=new FirefoxDriver();
-		// driver1.get("https://demowebshop.tricentis.com/");
+		 WebDriver driver2=new InternetExplorerDriver();
+		 
+	
+		 
+		 driver2.get("https://demowebshop.tricentis.com/");
+		driver1.get("https://demowebshop.tricentis.com/");
 		driver.get("https://demowebshop.tricentis.com/");
 		driver.manage().window().maximize();
 		String title=driver.getTitle();
@@ -21,8 +27,22 @@ public class Browser_launch {
 		String windowhandle=driver.getWindowHandle();
 		System.out.println(windowhandle);
 		String pagesource=driver.getPageSource();
-		System.out.println(pagesource);
+		//System.out.println(pagesource);
+		
+		
+		
+		driver1.manage().window().maximize();
+		String title1=driver1.getTitle();
+		System.out.println(title1);
+		String currenturl1=driver1.getCurrentUrl();
+		System.out.println(currenturl1);
+		String windowhandle1=driver1.getWindowHandle();
+		System.out.println(windowhandle1);
+		String pagesource1=driver1.getPageSource();
+		//System.out.println(pagesource1);
 		//driver.close();
+		
+		
 
 	}
 
